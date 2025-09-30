adb13a31cb2973b554dcdfb33be0bfb8c1b88fa0
Anders Homm <ledimestari@gmail.com>
2025-09-30 17:57:30 +0000

    correct domain

--- custom_components/homeassistant_storj_integration/const.py
+++ custom_components/homeassistant_storj_integration/const.py
@@ -4,5 +4,5 @@ from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "homeassistant-storj-integration"
+DOMAIN = "homeassistant_storj_integration"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
