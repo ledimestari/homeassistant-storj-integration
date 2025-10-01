fa7d265a0752802bc48099cb6958dffb7186b31a
Anders Homm <36168743+ledimestari@users.noreply.github.com>
2025-10-01 19:38:15 +0300

    Delete custom_components/homeassistant_storj_integration directory

--- custom_components/homeassistant_storj_integration/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for integration_blueprint."""
-
-from logging import Logger, getLogger
-
-LOGGER: Logger = getLogger(__package__)
-
-DOMAIN = "homeassistant_storj_integration"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
